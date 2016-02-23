931274ccd77a301881eacb15a34fcc64a51c3206
paco <antoniohernando88@gmail.com>
2016-02-23 12:31:27 +0100

    implemented detail and edit funtions

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -11,6 +11,8 @@ class CardsController < ApplicationController
   end
 
   def show
+    id=params[:id]
+    @jobopening=Jobopening.find id   
   end
 
   def dashboard
@@ -38,7 +40,6 @@ class CardsController < ApplicationController
     
   end
 
-
   def search
     jobopenings=Jobopening.all
     @jobopenings=[]
@@ -57,6 +58,41 @@ class CardsController < ApplicationController
         end
     end
     render :index
+  end
+  
+  def edit
+    @grouped_options = []
+    categories = Category.all 
+    professions = Profession.all
+     for category in categories
+        auxCategory = [category.tittle]
+       @grouped_options << auxCategory
+         auxOptions = []
+         auxCategory << auxOptions
+        for profession in professions
+          if category.id == profession.category.id
+            auxOptions << [profession.tittle, profession.id]
+          end
+        end
+     end
+    #id=params[:id]
+    id=2
+    @jobopening=Jobopening.find id   
+  end
+  
+  def update_jobopening
+    id=params[:id]
+    jobopening=Jobopening.find id.to_i 
+    jobopening.tittle=params[:tittle]
+    jobopening.description=params[:description]
+    #jobopening.image=params[:image]
+    jobopening.requirement=params[:requirement]
+    jobopening.salary_min=params[:salary_min]
+    jobopening.salary_max=params[:salary_max]
+    #jobopening.province=params[:province]
+    jobopening.profession=Profession.find params[:profession].to_i
+    jobopening.save
+    render :dashboard
   end
   
 end
